2c82dd98813e008bd33568ec33828cef9b8aba19
kido <baekido@gmail.com>
2021-06-06 23:51:09 +0900

    add readme

--- src/main/java/com/schooldevops/security_sample/configs/MySecurityConfig.java
+++ src/main/java/com/schooldevops/security_sample/configs/MySecurityConfig.java
@@ -1,32 +1,43 @@
 package com.schooldevops.security_sample.configs;
 
+import com.schooldevops.security_sample.security.MyAuthenticationProvider;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final PasswordEncoder passwordEncoder;
+    private final MyAuthenticationProvider authenticationProvider;
+
+    public MySecurityConfig(PasswordEncoder passwordEncoder, MyAuthenticationProvider authenticationProvider) {
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationProvider = authenticationProvider;
+    }
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//
+//        //  AuthorizationManager 를 생성한다. 여기서는 메모리에 적재해두고 사용할 것이기 때문에 InMemoryUserDetailManager를 이용했다.
+//        InMemoryUserDetailsManager userDetailService = new InMemoryUserDetailsManager();
+//
+//        //  User 를 지정한다.
+//        //  username: custom_user, password: custom_user_123 으로 지정했다.
+//        UserDetails user = User.withUsername("custom_user").password(passwordEncoder.encode("custom_user_123")).authorities("read").build();
+//        userDetailService.createUser(user);
+//
+//
+//        auth.userDetailsService(userDetailService).passwordEncoder(passwordEncoder);
+//    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        //  Authorization Provider 에서 사용할 PasswordEncoder 생성
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
-        //  AuthorizationManager 를 생성한다. 여기서는 메모리에 적재해두고 사용할 것이기 때문에 InMemoryUserDetailManager를 이용했다.
-        InMemoryUserDetailsManager userDetailService = new InMemoryUserDetailsManager();
-
-        //  User 를 지정한다.
-        //  username: custom_user, password: custom_user_123 으로 지정했다.
-        UserDetails user = User.withUsername("custom_user").password(passwordEncoder.encode("custom_user_123")).authorities("read").build();
-        userDetailService.createUser(user);
-
-
-        auth.userDetailsService(userDetailService).passwordEncoder(passwordEncoder);
+        auth.authenticationProvider(authenticationProvider);
     }
 
     @Override
@@ -37,5 +48,10 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
         //  인증된 요청만 통과하도록 지정한다.
         http.authorizeRequests().anyRequest().authenticated();
 
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/schooldevops/security_sample/security/MyAuthenticationProvider.java
@@ -0,0 +1,31 @@
+package com.schooldevops.security_sample.security;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class MyAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String userName = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        if ("custom_user".equals(userName) && "custom_user_123".equals(password) ) {
+            return new UsernamePasswordAuthenticationToken(userName, password, Arrays.asList());
+        } else {
+            throw new BadCredentialsException("Invalid Username or Password");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
